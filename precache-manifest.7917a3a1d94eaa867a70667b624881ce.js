5276ca9c3c15dd40e41ccb093cc681151b30a57e
ekunish <ekunish@gmail.com>
2020-08-27 23:29:08 +0900

    Updates

---
+++ precache-manifest.7917a3a1d94eaa867a70667b624881ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765d622c515a8b536d2699741e6fc3b1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c8981e2ac85567c7d735",
+    "url": "/monsters-rolodex/static/css/main.928a3bc5.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8981e2ac85567c7d735",
+    "url": "/monsters-rolodex/static/js/main.9fff5d52.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
